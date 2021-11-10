ff705ea9cb50e646688d6e95d3d2e21d7369b3f9
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-10 11:39:33 +0000

    Working with refs & useRef

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,8 +1,25 @@
+import React, { useRef } from 'react';
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // ? = value might be null, but that's okay
+    // ! = give me the concrete value. will not accept null
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
